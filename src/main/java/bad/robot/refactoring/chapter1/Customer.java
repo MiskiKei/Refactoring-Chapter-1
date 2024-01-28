48b2455ad8889372f9e7d8ce375a60fd58d1a2ec
MiskiKei <107701715+MiskiKei@users.noreply.github.com>
2024-01-28 01:06:39 -0500

    Rename Method: var frequentRenterPoints -> points

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -41,12 +41,12 @@ public class Customer {
     }
 
 	private int calculateFrequentRenterPoints(Rental rental) {
-		int frequentRenterPoints = 0;
+		int points = 0;
 		// add frequent renter points
-		frequentRenterPoints++;
+		points++;
 		// add bonus for a two day new release rental
 		if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-		    frequentRenterPoints++;
-		return frequentRenterPoints;
+			points++;
+		return points;
 	}
 }
